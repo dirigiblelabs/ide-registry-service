0e7c9f00bf8b03894f0a0ebb4de5d3b26afbe2da
StanZGenchev <stan.z.genchev@gmail.com>
2022-07-19 20:33:27 +0300

    Fixed API

--- ide-registry-service/registry.js
+++ ide-registry-service/registry.js
@@ -2,17 +2,7 @@ angular.module('ideRegistry', [])
     .provider('registryApi', function registryApiProvider() {
         this.registryServiceUrl = '/services/v4/core/registry';
         this.$get = ['$http', function registryApiFactory($http) {
-            let getMetadata = function (resourceUrl) {
-                return $http.get(resourceUrl, { headers: { 'describe': 'application/json' } })
-                    .then(function successCallback(response) {
-                        return { status: response.status, data: response.data };
-                    }, function errorCallback(response) {
-                        console.error('Registry service:', response);
-                        return { status: response.status };
-                    });
-            }
-
-            let loadregistry = function (resourcePath = '/') {
+            let loadRegistry = function (resourcePath = '/') {
                 let url = new UriBuilder().path(this.registryServiceUrl.split('/')).path(resourcePath.split('/')).build();
                 return $http.get(url, { headers: { 'describe': 'application/json' } })
                     .then(function successCallback(response) {
@@ -24,8 +14,7 @@ angular.module('ideRegistry', [])
             }.bind(this);
 
             return {
-                getMetadata: getMetadata,
-                load: loadregistry,
+                load: loadRegistry,
             };
         }];
     });
